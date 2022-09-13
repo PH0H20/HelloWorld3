bf30ae22620d2f198275d8c7f11886f2398e4664
PH0 - Ali <113189507+PH0H20@users.noreply.github.com>
2022-09-13 08:57:35 -0300

    Files added
    3

---
+++ HelloWord3/HelloWord3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello_World3");
+            Console.ReadKey(true);
+        }
+    }
+}
